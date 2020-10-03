09478fdb948003a99acffd50bfe130e64edad5a1
Andreizayaz <poznavauka@mail.ru>
2020-10-03 16:35:38 +0300

    feat: solve tasks carbon-dating.js and dream-team.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	if(typeof sampleActivity !== 'string' || sampleActivity==null ||
+	isNaN(sampleActivity) || +sampleActivity==0 ||
+	sampleActivity>HALF_LIFE_PERIOD ||
+	sampleActivity <1 ||
+	sampleActivity>15) return false;
+	
+	const logNumber = Math.log(MODERN_ACTIVITY/sampleActivity);
+	const k = .693/HALF_LIFE_PERIOD;
+	
+	const result = Math.ceil(logNumber/k);
+
+	return result;
 };
