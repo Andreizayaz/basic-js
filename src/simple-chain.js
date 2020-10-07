dd09629986ad4f8f3990f5d45992360838130cb5
Andreizayaz <poznavauka@mail.ru>
2020-10-07 18:34:16 +0300

    feat: first solve task simple-chain.js. doesn't work

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,64 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+	getLength() {
+		let objLength = 0;
+		for (key in this) {
+			if (key === 'getLength' || key === 'addLink' ||
+				key === 'removeLink' || key === 'reverseChain' ||
+				key === 'finishChain') continue;
+			objLength++;
+		}
+		return objLength;
+	},
+
+	addLink(value) {
+		let numberKey = this.getLength();
+		numberKey++;
+
+		if (!value) {
+			this[numberKey] = '( )';
+			return this;
+		}
+		this[numberKey] = value;
+		return this;
+	},
+
+	removeLink(position) {
+		if (typeof position !== 'number' || position % 1 !== 0 || position < 0 || position > this.getLength()) {
+			throw new Error();
+		}
+		delete this[position];
+		return this;
+	},
+
+	reverseChain() {
+		//chainMaker = Object.keys(this).reverse();
+		return this;
+	},
+
+	finishChain() {
+		let str = '';
+		let counter = 1;
+		let objectLength = this.getLength();
+		for (let key in this) {
+			if (key === 'getLength' || key === 'addLink' ||
+				key === 'removeLink' || key === 'reverseChain' ||
+				key === 'finishChain') continue;
+			if (objectLength === 0) {
+				str += '( )';
+				return str;
+			}
+			if (counter === objectLength) {
+				str += `( ${this[key]} )`;
+				return str;
+			}
+
+			str += `( ${this[key]} )~~`;
+			counter++;
+		}
+		return str;
+	}
 };
 
 module.exports = chainMaker;
