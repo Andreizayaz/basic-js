908ee08b691d6d8ee6fc7119bf82120bf61b8d6e
Andreizayaz <poznavauka@mail.ru>
2020-10-07 19:13:08 +0300

    fix: fix error in simle-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,63 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+	simpleChain: [],
+
 	getLength() {
-		let objLength = 0;
-		for (key in this) {
-			if (key === 'getLength' || key === 'addLink' ||
-				key === 'removeLink' || key === 'reverseChain' ||
-				key === 'finishChain') continue;
-			objLength++;
-		}
-		return objLength;
+		return this.simpleChain.length;
 	},
 
 	addLink(value) {
-		let numberKey = this.getLength();
-		numberKey++;
-
-		if (!value) {
-			this[numberKey] = '( )';
+		if (value === '' || value === undefined) {
+			this.simpleChain.push(`( )`);
 			return this;
 		}
-		this[numberKey] = value;
+		this.simpleChain.push(`( ${value} )`);
 		return this;
 	},
 
 	removeLink(position) {
-		if (typeof position !== 'number' || position % 1 !== 0 || position < 0 || position > this.getLength()) {
+		if (typeof position !== 'number' ||
+			position % 1 !== 0 || position < 0 ||
+			position > this.simpleChain.length - 1) {
+			this.simpleChain = [];
 			throw new Error();
 		}
-		delete this[position];
+		let removePosition = position - 1;
+		this.simpleChain.splice(removePosition, 1);
 		return this;
 	},
 
 	reverseChain() {
-		//chainMaker = Object.keys(this).reverse();
+		this.simpleChain.reverse();
 		return this;
 	},
 
 	finishChain() {
-		let str = '';
-		let counter = 1;
-		let objectLength = this.getLength();
-		for (let key in this) {
-			if (key === 'getLength' || key === 'addLink' ||
-				key === 'removeLink' || key === 'reverseChain' ||
-				key === 'finishChain') continue;
-			if (objectLength === 0) {
-				str += '( )';
-				return str;
-			}
-			if (counter === objectLength) {
-				str += `( ${this[key]} )`;
-				return str;
-			}
-
-			str += `( ${this[key]} )~~`;
-			counter++;
-		}
-		return str;
+		let finishSimpleChain = this.simpleChain;
+		this.simpleChain = [];
+		return finishSimpleChain.join('~~');
 	}
 };
 
