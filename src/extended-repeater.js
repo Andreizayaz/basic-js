9b1df2ed6b3c017f2664f21391416ac6a13bc89a
Andreizayaz <poznavauka@mail.ru>
2020-10-18 20:52:49 +0300

    feat: solve tasks extended-repeater.js and hanoi-tower.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, args) {
+	//str = str.toString();
+	let resultStr = '';
+	if(args.additionSeparator!==undefined) args.additionSeparator = args.additionSeparator.toString();
+	
+	/*if(typeof args.repeatTimes !== 'number' 
+	  && typeof args.additionRepeatTimes !== 'number') throw new Error();*/
   
+	  args.separator = args.separator || '+';
+	  args.additionSeparator=args.additionSeparator || '|';
+  
+	  //if(args.repeatTimes === undefined) return resultStr = str;
+  
+	  for (let i = 1; i <= args.repeatTimes; i++) {
+		  resultStr+=str;
+		  for (let j = 0; j < args.additionRepeatTimes; j++) {
+			  resultStr+=args.addition;
+			  if(j<args.additionRepeatTimes-1) resultStr+=args.additionSeparator;
+		  }
+		  
+		  if(i<args.repeatTimes)resultStr+=args.separator;
+	  }
+  
+	  if(args.repeatTimes===undefined){
+		  resultStr+=str;
+		  for (let j = 0; j < args.additionRepeatTimes; j++) {
+			  resultStr+=args.addition;
+			  if(j<args.additionRepeatTimes-1) resultStr+=args.additionSeparator;
+		  }
+		  if(args.additionRepeatTimes===undefined) resultStr+=args.addition;
+	  }
+  
+	  return resultStr;
+};
